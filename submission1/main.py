ad8a623731ae1f6ff49e7c16f1fc70daab031243
kyrashchuk3 <mikolakyrashuk769@gmail.com>
2026-03-19 21:28:04 +0200

    Implement basic arithmetic operations with user input

--- submission1/main.py
+++ submission1/main.py
@@ -1,1 +1,19 @@
-# Implement task here
+# Зчитування двох чисел
+a = float(input("Введіть перше число: "))
+b = float(input("Введіть друге число: "))
+
+# Обчислення
+sum_result = a + b
+sub_result = a - b
+mul_result = a * b
+
+# Виведення результатів
+print("Додавання:", sum_result)
+print("Віднімання:", sub_result)
+print("Множення:", mul_result)
+
+if b != 0:
+    div_result = a / b
+    print("Ділення:", div_result)
+else:
+    print("Ділення: неможливе (ділення на нуль)")
