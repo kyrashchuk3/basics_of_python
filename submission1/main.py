8aefe0c28576dd0c90ec452febb60b1fa0cf4eee
kyrashchuk3 <mikolakyrashuk769@gmail.com>
2026-03-19 21:41:14 +0200

    Update main.py

--- submission1/main.py
+++ submission1/main.py
@@ -1,19 +1,11 @@
-# Зчитування двох чисел
-a = float(input("Введіть перше число: "))
-b = float(input("Введіть друге число: "))
+a = float(input())
+b = float(input())
 
-# Обчислення
-sum_result = a + b
-sub_result = a - b
-mul_result = a * b
-
-# Виведення результатів
-print("Додавання:", sum_result)
-print("Віднімання:", sub_result)
-print("Множення:", mul_result)
+print(a + b)
+print(a - b)
+print(a * b)
 
 if b != 0:
-    div_result = a / b
-    print("Ділення:", div_result)
+    print(a / b)
 else:
-    print("Ділення: неможливе (ділення на нуль)")
+    print("Division by zero")
