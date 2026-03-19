39776c8a9dd629d5f9b9a39d0664e37f658702af
kyrashchuk3 <mikolakyrashuk769@gmail.com>
2026-03-19 21:48:58 +0200

    Create calculate function for arithmetic operations
    Refactored arithmetic operations into a function.

--- submission1/main.py
+++ submission1/main.py
@@ -1,11 +1,13 @@
-a = float(input())
-b = float(input())
-
-print(a + b)
-print(a - b)
-print(a * b)
-
-if b != 0:
-    print(a / b)
-else:
-    print("Division by zero")
+def calculate(a, b):
+    result = []
+    
+    result.append(a + b)
+    result.append(a - b)
+    result.append(a * b)
+    
+    if b != 0:
+        result.append(a / b)
+    else:
+        result.append("Division by zero")
+    
+    return result
